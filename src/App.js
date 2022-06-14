8b0c4426a10640a59d9e843e051a6e4da50b53ba
marcoacsena <36572272+marcoacsena@users.noreply.github.com>
2022-06-14 16:44:08 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Develop by Marco Sena.
+          Develop by Marco A.C. Sena.
         </p>
         <a
           className="App-link"
