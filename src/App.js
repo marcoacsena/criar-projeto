3753ab5b1d50548c5c50b5ef2d861bf98a756d6f
marcoacsena <marcoacsena@gmail.com>
2022-06-14 17:07:55 -0300

    App.js modificado no ambiente develop!!!!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Develop by Marco - React.
+          App.js modificado no ambiente develop!!!!
         </p>
         <a
           className="App-link"
